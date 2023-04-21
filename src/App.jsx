2f6a1c8149d9cae99ed467036ec56e2a0a460058
Yoran515 <113905605+Yoran515@users.noreply.github.com>
2023-04-21 14:36:41 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { Box } from "@react-three/drei";
+import { useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 
-function App() {
-  const [count, setCount] = useState(0)
+function Stage() {
+  const boxRef = useRef();
+
+  useFrame(() => {
+    boxRef.current.rotation.x += 0.01;
+    boxRef.current.rotation.y += 0.01;
+  });
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <>
+      <Box ref={boxRef} args={[1, 1, 1]} position={[0, 0, 0]}>
+        <meshStandardMaterial color="blue" />
+      </Box>
+    </>
+  );
 }
 
-export default App
+function App() {
+  const [show, setShow] = useState(0);
+
+  return (
+    <>
+      <button onClick={() => setShow((show) => !show)}>Click me</button>
+      <Canvas
+        camera={{
+          position: [0, 2, 10],
+          fov: 75,
+        }}
+      >
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        {show && <Stage />}
+      </Canvas>
+    </>
+  );
+}
+export default App;
